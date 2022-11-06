e6bba6f1f66d64742cf2945b2b8931cbc9613513
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-05 23:46:19 -0500

    subreddit_stats

---
+++ subreddit_stats.sql
@@ -0,0 +1,51 @@
+CREATE TABLE subreddit_stats_link AS
+SELECT
+    playlist_path subreddit,
+    MIN(time_created) oldest,
+    CAST(AVG(time_created) AS INT) avg_age,
+    MAX(time_created) latest,
+    CAST(AVG(time_created - NULLIF(time_modified, 0)) AS INT) avg_time_to_edit,
+    MIN(score) min_score,
+    AVG(score) avg_score,
+    MAX(score) max_score,
+    AVG(num_comments) avg_comments,
+    MAX(num_comments) max_comments,
+    AVG(num_crossposts) avg_crossposts,
+    COUNT(path) COUNT,
+    COUNT(
+        DISTINCT author
+    ) count_authors,
+    AVG(LENGTH(path)) avg_length,
+    AVG(LENGTH(title)) avg_length_title,
+    AVG(upvote_ratio) avg_upvote_ratio,
+    AVG(COALESCE(is_over_18, 0)) is_over_18_ratio
+FROM
+    media
+GROUP BY
+    subreddit;
+
+CREATE TABLE subreddit_stats_text AS
+SELECT
+    playlist_path subreddit,
+    MIN(time_created) oldest,
+    CAST(AVG(time_created) AS INT) avg_age,
+    MAX(time_created) latest,
+    CAST(AVG(time_created - NULLIF(time_modified, 0)) AS INT) avg_time_to_edit,
+    MIN(score) min_score,
+    AVG(score) avg_score,
+    MAX(score) max_score,
+    AVG(num_comments) avg_comments,
+    MAX(num_comments) max_comments,
+    AVG(num_crossposts) avg_crossposts,
+    COUNT(path) COUNT,
+    COUNT(
+        DISTINCT author
+    ) count_authors,
+    AVG(LENGTH(selftext)) avg_length,
+    AVG(LENGTH(title)) avg_length_title,
+    AVG(upvote_ratio) avg_upvote_ratio,
+    AVG(COALESCE(is_over_18, 0)) is_over_18_ratio
+FROM
+    reddit_posts
+GROUP BY
+    subreddit;
